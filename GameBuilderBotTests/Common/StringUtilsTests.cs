f24fa0659bbf3f9179bb7be080ae15762c6c084f
Monique <monique@bounceswoosh.org>
2025-04-19 20:25:36 -0600

    a few tests

---
+++ GameBuilderBotTests/Common/StringUtilsTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GameBuilderBot.Common.Tests
+{
+    [TestClass()]
+    public class StringUtilsTests
+    {
+        [TestMethod()]
+        public void SanitizeForFileNameTest()
+        {
+            char c = ' ';
+            string invalidString = StringUtils.SanitizeForFileName(" ");
+            if (!invalidString.Equals("_"))
+            {
+                Assert.Fail();
+            }
+
+            c = System.IO.Path.GetInvalidFileNameChars()[0];
+            invalidString = StringUtils.SanitizeForFileName(c.ToString());
+            if (!invalidString.Equals("_"))
+            {
+                Assert.Fail();
+            }
+
+            string validString = "HiHowAreYou";
+            if (!validString.Equals("HiHowAreYou"))
+            {
+                Assert.Fail();
+            }
+
+
+        }
+    }
+}
