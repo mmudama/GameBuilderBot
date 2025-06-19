d7f4b286b7a45596a40c7c0383b1e612af3d42f5
mmudama <mmudama@users.noreply.github.com>
2025-06-18 20:33:10 -0600

    Remove unnecessary try/catch that was causing a warning on build

--- GameBuilderBot/Runners/AssignmentRunner.cs
+++ GameBuilderBot/Runners/AssignmentRunner.cs
@@ -37,48 +37,41 @@ namespace GameBuilderBot.Runners
         /// <returns></returns>
         public (object, object) CalculateFieldValue(string[] FieldNameAndValue, SocketCommandContext discordContext)
         {
-            try
+            if (FieldNameAndValue.Length != 2)
             {
-                if (FieldNameAndValue.Length != 2)
-                {
-                    throw new GameBuilderBotException(OneLinerHelp());
-                }
-
-                GameState state = _gameService.GetGameStateForActiveGame(discordContext.Channel.Id);
-
-                string fieldName = FieldNameAndValue[0].ToLower();
-                string expression = FieldNameAndValue[1];
-                int value = CalculateValue(state, fieldName, expression);
-
-                if (int.TryParse(expression, out _))
-                {
-                    // The second user parameter was an explicit integer value, not an expression
-                    expression = null;
-                }
-
-                object oldValue = null;
-
-                if (state.FieldHasValue(fieldName))
-                {
-                    oldValue = state.Fields[fieldName].Value;
-                }
-
-                if (state.Fields.ContainsKey(fieldName))
-                {
-                    state.Fields[fieldName].Value = value;
-                }
-                else
-                {
-                    state.Fields[fieldName] = new Field(expression, value.ToString());
-                }
-
-                _exportService.ExportGameState(state, discordContext);
-                return (oldValue, state.Fields[fieldName].Value);
+                throw new GameBuilderBotException(OneLinerHelp());
             }
-            catch (Exception e)
+
+            GameState state = _gameService.GetGameStateForActiveGame(discordContext.Channel.Id);
+
+            string fieldName = FieldNameAndValue[0].ToLower();
+            string expression = FieldNameAndValue[1];
+            int value = CalculateValue(state, fieldName, expression);
+
+            if (int.TryParse(expression, out _))
             {
-                throw e;
+                // The second user parameter was an explicit integer value, not an expression
+                expression = null;
             }
+
+            object oldValue = null;
+
+            if (state.FieldHasValue(fieldName))
+            {
+                oldValue = state.Fields[fieldName].Value;
+            }
+
+            if (state.Fields.ContainsKey(fieldName))
+            {
+                state.Fields[fieldName].Value = value;
+            }
+            else
+            {
+                state.Fields[fieldName] = new Field(expression, value.ToString());
+            }
+
+            _exportService.ExportGameState(state, discordContext);
+            return (oldValue, state.Fields[fieldName].Value);
         }
     }
 }
